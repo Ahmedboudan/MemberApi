8a51447a7d2893cf66ed5f07001d0bc34b4af2be
Ahmed BOUDAN <ahmedboudan20@gmail.com>
2024-01-08 14:36:49 +0100

    first commit

---
+++ Controllers/MembreController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Membre.Data.Interface;
+using Membre.Data.Repository;
+
+namespace MembreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembreController : ControllerBase
+    {
+        private readonly IMembre membres = new MembreRepository();
+        [HttpGet("getall")]
+        public IActionResult GetAllMembres()
+        {
+            return Ok(membres.GetAllMembre());
+        }
+        [HttpGet("getmembrebyid/{id}")]
+        public IActionResult GetMenbere(int id)
+        {
+            return Ok(membres.GetMembre(id));
+        }
+    }
+}
